456c95a6f474f0ecc6b34569d2d213c925580929
Mathieu Rolland <mathieu.rolland@edu.escpeurope.eu>
2016-09-22 16:05:30 +0200

    les routes de sign up a gérer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @array = ["http://img.wallpaperfolder.com/f/4AC6CEC2B6CA/stock-exchange.jpg", "https://static.pexels.com/photos/25970/pexels-photo.jpg"]
   end
 
   def actus
